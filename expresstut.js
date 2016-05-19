09b2a2bb6ed6a8f456826797aa9736abe4a48126
Stephan Ng <stephann@stephann-macbookpro.roam.corp.google.com>
2016-05-19 16:34:56 -0700

    Stopped at ~27:50 of Derek Banas tutorial.

--- expresstut.js
+++ expresstut.js
@@ -2,12 +2,56 @@ var express = require('express');
 
 var app = express();
 
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// MORE IMPORTS HERE
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', function(req, res){
-	res.send('Express Works');
+	res.render('home');
 });
 
+app.use(function(req, res, next){
+	console.log("Looking for URL : " + req.url);
+	next();
+});
+
+app.get('/junk', function(req, res, next){
+	console.log('Tried to access /junk');
+	throw new Error('/junk doesn\'t exist');
+});
+
+app.use(function(err, req, res, next){
+	console.log('Error : ' + err.message);
+	next();
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+app.use(function(req, res){
+	res.type('text/html');
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+
+
 app.listen(app.get('port'), function(){
-	console.log('Express started press Ctrl-c to terminate');
+	console.log("Express started on http://localhost:" + app.get('port') + 'press Ctrl-C to terminate.');
 });
